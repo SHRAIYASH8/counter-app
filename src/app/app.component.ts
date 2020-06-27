44338e949d49ad5f07d91c07fcf526a814011acb
Kumar Shraiyash <justshraiyash@gmail.com>
2020-06-27 21:04:54 +0530

    component change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Counter App';
+  title: string = 'Counter Application';
   
   count: number = 0;
 
